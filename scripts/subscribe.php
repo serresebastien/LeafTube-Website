151a28c9020863e7668b7f5aa25e547bd7d1b5af
= <=>
2019-03-23 16:21:11 +0100

    timeline et description projet

--- scripts/subscribe.php
+++ scripts/subscribe.php
@@ -4,21 +4,21 @@
 //               COLLECTING DATA
 // ##########################################
 
-$email = $_POST['subscribe_email'];
-$myDate = date('l jS \of F Y h:i:s A');
+// $email = $_POST['subscribe_email'];
+// $myDate = date('l jS \of F Y h:i:s A');
 
 // ##########################################
 //               SENDING DATA
 // ##########################################
 
-mail("contact.leaftube@gmail.com","LeaftTube - Inscription à la newsletter","Merci d'ajouter $email à la mail list.");
+// mail("contact.leaftube@gmail.com","LeaftTube - Inscription à la newsletter","Merci d'ajouter $email à la mail list.");
 
 // ##########################################
 //					SAVING DATA
 // ##########################################
 
-$myFile = fopen("newsletter.txt", "a+");
-fwrite($myFile, "This subscription was receive on ".$myDate.".\n"$email."\n\n");
-fclose($myFile);
+// $myFile = fopen("newsletter.txt", "a+");
+// fwrite($myFile, "This subscription was receive on ".$myDate.".\n"$email."\n\n");
+// fclose($myFile);
 
 ?>
